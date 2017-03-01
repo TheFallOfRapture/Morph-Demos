9d57d55cdf710d1f5717bf4ad972379a6ede7622
Fernando Gonzalez <fallofrapture59@gmail.com>
2017-02-28 23:10:05 -0500

    Moved files into proper directory

--- shooting/controller/Command.java
+++
@@ -1,8 +0,0 @@
-package com.fate.game.shooting.controller;
-
-import com.fate.engine.entities.Entity;
-
-@FunctionalInterface
-public interface Command {
-	void execute(Entity e);
-}
--- shooting/controller/MovementCommand.java
+++
@@ -1,8 +0,0 @@
-package com.fate.game.shooting.controller;
-
-import com.fate.engine.entities.Entity;
-import com.fate.engine.math.Vector2f;
-
-public abstract class MovementCommand {
-	public abstract Vector2f addMovement(Entity e);
-}
